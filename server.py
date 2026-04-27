9aab692b606cac1ac0adbb5871b17cfcb313385b
Faalrushaid-KW <fahadaralrushaid@gmail.com>
2026-04-27 17:34:07 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,30 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 @app.route("/emotionDetector", methods=["GET"])
 def detect_emotion():
     text_to_analyze = request.args.get("textToAnalyze")
 
-    if text_to_analyze is None or text_to_analyze.strip() == "":
-        return jsonify({"error": "Invalid input! Try again."}), 400
-
     result = emotion_detector(text_to_analyze)
-    return jsonify(result)
+
+    if result["dominant_emotion"] is None:
+        return "Invalid input! Try again."
+
+    return f"""
+    For the given statement, the system response is:
+    anger: {result['anger']},
+    disgust: {result['disgust']},
+    fear: {result['fear']},
+    joy: {result['joy']},
+    sadness: {result['sadness']}.
+    The dominant emotion is {result['dominant_emotion']}.
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
